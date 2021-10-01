a833e6fdea92d82792f24c572217c44b46ffd2e3
Tomer Lichtash <tomer@konzepz.com>
2021-10-01 18:04:12 +0300

    formatting tests

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextTranslate = require('next-translate');
 
 module.exports = {
   webpack5: true,
+  compress: false,
+  optimization: {
+    minimizer: [],
+    chunkIds: 'named',
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
