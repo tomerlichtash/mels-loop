a00dba9f1fa327b9fa40a79b7cd1ecf177ce6c01
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2021-10-23 09:35:43 +0300

    cleanup

--- src/components/browser.tsx
+++ src/components/browser.tsx
@@ -2,17 +2,19 @@
 // 1. Use driver method to fetch codex
 // 2. map codex entries to display full text
 // 3. Handle annotations
+export interface ICodexEntry {
+	title: string;
+}
+
 export const Browser = ({
 	content
 }: {
-	content: unknown[];
+	content: ICodexEntry[];
 }): JSX.Element => {
 	return (
 		<div>
-			<h1>All content</h1>
-			{
-				content.map((rec, index) => <div key={index}>{(rec as any).title}</div>)
-			}
+			<h1>Codex content</h1>
+			{content.map((rec, index) => <div key={index}>{rec.title}</div>)}
 		</div>
 	);
 };
