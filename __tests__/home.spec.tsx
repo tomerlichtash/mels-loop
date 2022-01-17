30d6950959f6491a32bdaf6bb27a6f20204aa406
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-01-17 13:51:40 +0200

    pass sample test

--- __tests__/home.spec.tsx
+++ __tests__/home.spec.tsx
@@ -3,22 +3,24 @@ import ContentComponent from "../src/components/content/contentComponent";
 import {IContentComponentInitData, MLParsedNodeType} from '../src/interfaces/models'
 import {render, screen} from '@testing-library/react'
 describe('hello world', () => {
-  const compData = {
-    data: {
-      type: 'paragraph',
-      key: 'key',
-      line: 1,
-      children: [
-        {
-          type: 'text',
-          key: 'line-1',
-          line: 1,
-          text: 'sample line'
-        }
-      ]
-    },
-    locale: 'en-US'
-  } as IContentComponentInitData;
-  const wrapper = render(<ContentComponent data={compData}/>);
-  expect(wrapper.container.getElementsByTagName('span')[0]).toEqual(`sample line`);
+  it('should render sample line', () => {
+    const compData = {
+      data: {
+        type: 'paragraph',
+        key: 'key',
+        children: [
+          {
+            type: 'text',
+            key: 'line-1',
+            line: 1,
+            text: 'sample line'
+          }
+        ]
+      },
+      locale: 'en-US'
+    } as IContentComponentInitData;
+    const wrapper = render(<ContentComponent data={compData}/>);
+
+    expect(wrapper.container.querySelector('span').textContent).toContain(`sample line`);
+  });
 });
