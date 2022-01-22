bef49265def69c2f9702f30a249877e08c3c2e1d
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-01-22 12:10:23 +0200

    font preload with _document

--- src/locales/locales.ts
+++ src/locales/locales.ts
@@ -18,8 +18,6 @@ export interface ILocaleKeys {
 	SITE_SUBTITLE: "SITE_SUBTITLE";
 	LOCALE_SELECTOR_TITLE: "LOCALE_SELECTOR_TITLE";
 	LOCALE_SELECTOR_CLOSE: "LOCALE_SELECTOR_CLOSE";
-	LOCALE_LABEL_EN: "LOCALE_LABEL_EN";
-	LOCALE_LABEL_HE: "LOCALE_LABEL_HE";
 	MOBILE_MENU_OPEN_LABEL: "MOBILE_MENU_OPEN_LABEL";
 	MOBILE_MENU_CLOSE_LABEL: "MOBILE_MENU_CLOSE_LABEL";
 	ERROR_404_FILE_NOT_FOUND: "ERROR_404_FILE_NOT_FOUND";
@@ -30,39 +28,33 @@ export const LOCALE_LABELS: Record<keyof ILocaleName, string> = {
 	LOCALE_LABEL_HE: "עברית",
 };
 
-export const EN_US: Record<keyof ILocaleKeys, string> = Object.assign(
-	{
-		HOME_NAV_LABEL: "Home",
-		ABOUT_NAV_LABEL: "About",
-		STORY_NAV_LABEL: "The Story",
-		SITE_NAME: "Mel's Loop",
-		SITE_SUBTITLE: "A Comprehensive Guide to The Story of Mel",
-		LOCALE_SELECTOR_TITLE: "Choose Language",
-		LOCALE_SELECTOR_CLOSE: "Close",
-		MOBILE_MENU_OPEN_LABEL: "Menu",
-		MOBILE_MENU_CLOSE_LABEL: "Close",
-		ERROR_404_FILE_NOT_FOUND: "404",
-	},
-	LOCALE_LABELS
-);
+export const EN_US: Record<keyof ILocaleKeys, string> = {
+	HOME_NAV_LABEL: "Home",
+	ABOUT_NAV_LABEL: "About",
+	STORY_NAV_LABEL: "The Story",
+	SITE_NAME: "Mel's Loop",
+	SITE_SUBTITLE: "A Comprehensive Guide to The Story of Mel",
+	LOCALE_SELECTOR_TITLE: "Choose Language",
+	LOCALE_SELECTOR_CLOSE: "Close",
+	MOBILE_MENU_OPEN_LABEL: "Menu",
+	MOBILE_MENU_CLOSE_LABEL: "Close",
+	ERROR_404_FILE_NOT_FOUND: "404",
+};
 
-export const HE_IL: Record<keyof ILocaleKeys, string> = Object.assign(
-	{
-		HOME_NAV_LABEL: "שער",
-		ABOUT_NAV_LABEL: "אודות",
-		STORY_NAV_LABEL: "הסיפור",
-		SITE_NAME: "לולאת מל",
-		SITE_SUBTITLE: "המדריך המקיף לסיפור על מל",
-		LOCALE_SELECTOR_TITLE: "בחירת שפה",
-		LOCALE_SELECTOR_CLOSE: "סגירה",
-		MOBILE_MENU_OPEN_LABEL: "תפריט",
-		MOBILE_MENU_CLOSE_LABEL: "סגירה",
-		ERROR_404_FILE_NOT_FOUND: "404",
-	},
-	LOCALE_LABELS
-);
+export const HE_IL: Record<keyof ILocaleKeys, string> = {
+	HOME_NAV_LABEL: "שער",
+	ABOUT_NAV_LABEL: "אודות",
+	STORY_NAV_LABEL: "הסיפור",
+	SITE_NAME: "לולאת מל",
+	SITE_SUBTITLE: "המדריך המקיף לסיפור על מל",
+	LOCALE_SELECTOR_TITLE: "בחירת שפה",
+	LOCALE_SELECTOR_CLOSE: "סגירה",
+	MOBILE_MENU_OPEN_LABEL: "תפריט",
+	MOBILE_MENU_CLOSE_LABEL: "סגירה",
+	ERROR_404_FILE_NOT_FOUND: "404",
+};
 
 export const Locale: IAvailableLocales = {
-	EN_US,
-	HE_IL,
+	EN_US: Object.assign(EN_US, LOCALE_LABELS),
+	HE_IL: Object.assign(HE_IL, LOCALE_LABELS),
 };
---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com"></link>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossorigin
+					></link>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Heebo:wght@100&family=Nunito:wght@300&display=swap"
+						rel="stylesheet"
+					></link>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
