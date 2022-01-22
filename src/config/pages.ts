a1f72f2ebd3f75001a112c0594dbefe399bd96fd
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-01-22 11:12:51 +0200

    update resources md

--- src/config/pages.ts
+++ src/config/pages.ts
@@ -2,4 +2,9 @@ export const SITE_PAGES = [
 	{ label: "HOME_NAV_LABEL", id: "home", targetPathname: "/" },
 	{ label: "STORY_NAV_LABEL", id: "story", targetPathname: "/story" },
 	{ label: "ABOUT_NAV_LABEL", id: "about", targetPathname: "/about" },
+	{
+		label: "RESOURCES_NAV_LABEL",
+		id: "resources",
+		targetPathname: "/docs/resources",
+	},
 ];
