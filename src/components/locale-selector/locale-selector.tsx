caab37215fe2279fd9baa100015fa6d5b19fbd7e
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-01-24 12:15:18 +0200

    remove any

--- src/components/locale-selector/locale-selector.tsx
+++ src/components/locale-selector/locale-selector.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import DropDown from "../dropdown";
 import { style, classes } from "./locale-selector.st.css";
 import { ComponentProps } from "../../interfaces/models";
+import { IOption } from "../dropdown/option";
 
 export interface LocaleSelectorProps extends ComponentProps {
 	currentLocaleLabel: string;
-	options: any[];
+	options: IOption[];
 }
 
 export const LocaleSelector = (props: LocaleSelectorProps): JSX.Element => {
