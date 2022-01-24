d58fbb6db27bd95271caeb8f5bdf7e03bdba3cfe
dfl <dfl.github@gmail.com>
2022-01-24 16:07:58 +0200

    Added documentation

--- src/lib/next-utils.ts
+++ src/lib/next-utils.ts
@@ -6,9 +6,12 @@ import { loadContentFolder } from "./markdown-driver";
 ////////////// Extended Next.js types //////////////////
 /**
  * Same as Next's GetStaticProps, parameterized by a content folder relative path
+ * Will load either the index in the folder, or all the indices in the child folders,
+ * depending on the type parameter
+ * @param type `"folder"`: scan the index in the folder, `"children"`: scan indices in child folders
  */
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-export type MLGetStaticProps<
+type MLGetStaticProps<
 	P extends { [key: string]: any } = { [key: string]: any },
 	Q extends ParsedUrlQuery = ParsedUrlQuery,
 	D extends PreviewData = PreviewData
@@ -21,23 +24,34 @@ export type MLGetStaticProps<
 /**
  * Same as Next's GetStaticProps, parameterized by a content folder relative path
  */
- export type MLGetStaticPaths<P extends ParsedUrlQuery = ParsedUrlQuery> = (
+type MLGetStaticPaths<P extends ParsedUrlQuery = ParsedUrlQuery> = (
 	folderRelativePath: string,
 	context: GetStaticPathsContext
 ) => Promise<GetStaticPathsResult<P>> | GetStaticPathsResult<P>
 
 export interface IMLNextUtils {
+	/**
+	 * Same as Next's GetStaticProps, parameterized by a content folder relative path
+	 * Will load either the index in the folder, or all the indices in the child folders,
+	 * depending on the type parameter
+	 * @param folderRelativePath The folder path relative to the content folder
+	 * @param ctx The original context passed to the static getStaticProps function
+	 * @param type `"folder"`: scan the index in the folder, `"children"`: scan indices in child folders
+	 */
 	getFolderStaticProps: MLGetStaticProps;
+	/**
+	 * Same as Next's GetStaticPaths, parameterized by a content folder relative path
+	 */
 	getFolderStaticPaths: MLGetStaticPaths;
 }
 
 class MLNextUtils implements IMLNextUtils {
-	getFolderStaticProps(folderPath: string, context: GetStaticPropsContext, type: "folder" | "children"): 
+	getFolderStaticProps(folderPath: string, context: GetStaticPropsContext, type: "folder" | "children"):
 		GetStaticPropsResult<{ [key: string]: any }> {
 		const docData = loadContentFolder({
 			relativePath: folderPath,
 			locale: context.locale,
-			type 
+			type
 		});
 		return {
 			props: {
@@ -50,16 +64,16 @@ class MLNextUtils implements IMLNextUtils {
 		context: GetStaticPathsContext): GetStaticPathsResult<ParsedUrlQuery> {
 		const paths: ILocaleMap[] = [];
 		(context.locales || []).forEach((locale: string) => {
-			const folderData = loadContentFolder({locale, relativePath: folderPath, type: "children"});
+			const folderData = loadContentFolder({ locale, relativePath: folderPath, type: "children" });
 			paths.push(...folderData.ids);
 		})
 		return {
 			paths,
 			fallback: false,
 		};
-	
+
 	}
-	
+
 }
 
 export const mlNextUtils: IMLNextUtils = new MLNextUtils();
