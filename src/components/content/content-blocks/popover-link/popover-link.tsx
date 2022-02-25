874732ea06a78885243181dbacf069aa5bbdacd2
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-02-25 22:31:02 +0200

    added stylable class names to popover

--- src/components/content/content-blocks/popover-link/popover-link.tsx
+++ src/components/content/content-blocks/popover-link/popover-link.tsx
@@ -5,25 +5,23 @@ import * as Popover from "@radix-ui/react-popover";
 import { classes } from "./popover-link.st.css";
 
 export const PopoverLink = (props: ContentComponentProps): JSX.Element => {
-	const p = props.componentData.node;
+	// const p = props.componentData.node;
 	return (
-		<Popover.Root>
-			<Popover.Trigger className={classes.popOverTrigger} asChild>
-				<span className={classes.popOverTriggerChild}>
-					<ContentIterator componentData={props.componentData} />
-				</span>
-			</Popover.Trigger>
-			<Popover.Content
-				side="top"
-				align="center"
-				className={classes.popOverContent}
-			>
-				<h1>title</h1>
-				<p>some text some text some text some text some text some text </p>
-				<Popover.Close>Close</Popover.Close>
-				<Popover.Arrow className={classes.popOverArrow} />
-			</Popover.Content>
-		</Popover.Root>
+		<div className={classes.root}>
+			<Popover.Root>
+				<Popover.Trigger className={classes.trigger} asChild>
+					<span className={classes.triggerInner}>
+						<ContentIterator componentData={props.componentData} />
+					</span>
+				</Popover.Trigger>
+				<Popover.Content side="top" align="center" className={classes.content}>
+					<h1>title</h1>
+					<p>some text</p>
+					<Popover.Close className={classes.closeButton}>Close</Popover.Close>
+					<Popover.Arrow className={classes.arrow} />
+				</Popover.Content>
+			</Popover.Root>
+		</div>
 	);
 };
 
