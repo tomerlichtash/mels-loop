7ab5f720c31a3f3f6650d3321abed1075f98855d
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-02-25 19:08:11 +0200

    added radix ui popover to popover-link

--- src/components/content/content-blocks/popover-link/popover-link.tsx
+++ src/components/content/content-blocks/popover-link/popover-link.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { ContentComponentProps } from "../../../../interfaces/models";
 import { ContentIterator } from "../../content-iterator";
+import * as Popover from "@radix-ui/react-popover";
 import { classes } from "./popover-link.st.css";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
 
 export const PopoverLink = (props: ContentComponentProps): JSX.Element => {
 	const p = props.componentData.node;
 	return (
-		<a
-			className={classes.root}
-			href={p.target}
-			target="_blank"
-			rel="noreferrer"
-		>
-			<ContentIterator componentData={props.componentData} />
-		</a>
+		<Popover.Root>
+			<Popover.Trigger className={classes.popOverTrigger} asChild>
+				<span>
+					<ContentIterator componentData={props.componentData} />
+				</span>
+			</Popover.Trigger>
+			<Popover.Content
+				side="top"
+				align="center"
+				className={classes.popOverContent}
+			>
+				<h1>title</h1>
+				<p>some text some text some text some text some text some text </p>
+				<Popover.Close>Close</Popover.Close>
+				<Popover.Arrow className={classes.popOverArrow} />
+			</Popover.Content>
+		</Popover.Root>
 	);
 };
 
--- src/components/popover/popover.tsx
+++ src/components/popover/popover.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ComponentProps } from "../../interfaces/models";
+import * as Popover from "@radix-ui/react-popover";
 import { style, classes } from "./popover.st.css";
 
 export interface PopOverProps extends ComponentProps {
@@ -16,8 +17,20 @@ export const PopOver = (props: PopOverProps): JSX.Element => {
 			onMouseOver={() => togglePop(true)}
 			onMouseLeave={() => togglePop(false)}
 		>
-			{showPop && <div className={classes.popBox}>{props["data-content"]}</div>}
-			{props.children}
+			{/* {showPop && <div className={classes.popBox}>{props["data-content"]}</div>} */}
+			<Popover.Root>
+				<Popover.Trigger className={classes.popOverTrigger}>
+					{props.children}
+				</Popover.Trigger>
+				<Popover.Content side="top" align="center">
+					<div className={classes.popOverContent}>
+						<h1>title</h1>
+						<p>some text some text some text some text some text some text </p>
+						<Popover.Close>Close</Popover.Close>
+					</div>
+					<Popover.Arrow />
+				</Popover.Content>
+			</Popover.Root>
 		</div>
 	);
 };
