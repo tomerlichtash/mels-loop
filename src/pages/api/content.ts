16426a65de390e49c8e589435fbccc2d13475c33
dfl <dfl.github@gmail.com>
2022-03-07 01:27:16 +0200

    /api/content handles both glossary and annotations

--- src/pages/api/content.ts
+++ src/pages/api/content.ts
@@ -4,18 +4,30 @@ import { MLParseModes } from "../../interfaces/models";
 import { loadContentFolder, LoadContentModes } from "../../lib/markdown-driver";
 import { LoadFolderModes } from "../../lib/next-utils";
 
+const TypeMap = {
+	annotation: CONTENT_TYPES.ANNOTATION,
+	glossary: CONTENT_TYPES.GLOSSARY
+};
+
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-	const locale = _req.query?.locale || "en";
+	const locale = String(_req.query.locale || "");
+	const type = String(_req.query.type || "");
+	const contentType = type && TypeMap[type];
+	if (!locale || !contentType) {
+		return res.status(500).json({ error: `Bad content params, locale ${locale} type ${type} 
+(expected one of ${Object.keys(TypeMap).toString()})`});
+	}
 	const loadPromise = new Promise((resolve) => {
 		const docData = loadContentFolder({
-			relativePath: CONTENT_TYPES.GLOSSARY,
-			locale: locale as string,
+			relativePath: contentType,
+			locale: locale,
 			contentMode: LoadContentModes.FULL,
 			loadMode: LoadFolderModes.CHILDREN,
 			parseMode: MLParseModes.NORMAL,
 		});
 		resolve({
 			locale,
+			// turn array into map
 			items: docData.pages.reduce((acc, pageData) => {
 				acc[pageData.id] = pageData;
 				return acc;
