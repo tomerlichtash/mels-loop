fbd19f9f225c85828070c532ac6dba92c56ba505
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-03-06 17:00:10 +0200

    annotations and glossary in hebrew codex

--- src/pages/api/glossary.ts
+++ src/pages/api/glossary.ts
@@ -1,30 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { CONTENT_TYPES } from "../../consts";
 import { MLParseModes } from "../../interfaces/models";
 import { loadContentFolder, LoadContentModes } from "../../lib/markdown-driver";
 import { LoadFolderModes } from "../../lib/next-utils";
 
-export default function handler(_req, res) {
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
 	const locale = _req.query?.locale || "en";
 	const loadPromise = new Promise((resolve) => {
 		const docData = loadContentFolder({
 			relativePath: CONTENT_TYPES.GLOSSARY,
-			locale,
+			locale: locale as string,
 			contentMode: LoadContentModes.FULL,
 			loadMode: LoadFolderModes.CHILDREN,
-			parseMode: MLParseModes.NORMAL
+			parseMode: MLParseModes.NORMAL,
 		});
 		resolve({
 			locale,
 			items: docData.pages.reduce((acc, pageData) => {
-				acc[pageData.id] = pageData
+				acc[pageData.id] = pageData;
 				return acc;
-			}, {})
+			}, {}),
 		});
 	});
-	loadPromise.then(data => {
-		res.status(200).json({ data });
-	})
-		.catch(error => {
-			res.status(500).json({ error: String(error) });
+	loadPromise
+		.then((data) => {
+			res.status(200).json({ data });
 		})
+		.catch((error) => {
+			res.status(500).json({ error: String(error) });
+		});
 }
