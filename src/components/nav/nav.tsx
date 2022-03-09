5c275d9586056e45f96f979b9c53d31b918da817
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-03-09 10:09:18 +0200

    cleanup

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -3,7 +3,6 @@ import { Button } from "../ui";
 import { ComponentProps } from "../../interfaces/models";
 import { ReactLayoutContext } from "../../contexts/layout-context";
 import { style, classes } from "./nav.st.css";
-import { getDisplayName } from "next/dist/shared/lib/utils";
 
 export const Nav = ({ className }: ComponentProps): JSX.Element => {
 	const {
