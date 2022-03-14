5f0cadec509d7ac95336879a6784131e45a8fdee
dfl <dfl.github@gmail.com>
2022-03-14 17:58:17 +0200

    Missing API

---
+++ src/pages/api/test.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import * as fs from "fs";
+import * as fsPath from "path";
+/* eslint-disable */
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+	try {
+		testIt().then(result => {
+			res.status(200).json(result);
+		});
+	}
+	catch (e) {
+		res.status(200).json({ error: String(e) });
+	}
+}
+
+async function testIt() {
+	const p = new Promise((resolve => {
+		fs.mkdtemp("ml", (err, folder) => {
+			if (err) {
+				resolve({ error: `mkdtemp error ${String(err)}` })
+			}
+			const filePath = fsPath.join(folder, "rabak.json");
+			fs.writeFile(filePath, JSON.stringify({ name: "maor" }), (err => {
+				if (err) {
+					resolve({ error: `write file error ${String(err)}` })
+				}
+				fs.readFile(filePath, (err, data) => {
+					if (err) {
+						resolve({ error: `read file error ${String(err)}` })
+					}
+					resolve(JSON.parse(data.toString()));
+				})
+			}))
+		})
+	}));
+	try {
+		const result = await p;
+		return result;
+	}
+	catch (e) {
+		return { error: String(e) }
+	}
+}
