a84be9bf489b4f9e2e02fe8797f21eca07e0b198
dfl <dfl.github@gmail.com>
2022-03-16 10:53:19 +0200

    temp name for temp favicon Replaced containing span in AnnotationLink with the react <> construct

--- src/components/content/content-blocks/annotation-link/annotation-link.tsx
+++ src/components/content/content-blocks/annotation-link/annotation-link.tsx
@@ -4,9 +4,9 @@ import { ContentComponentProps } from "../../../../interfaces/models";
 export const AnnotationLink = (props: ContentComponentProps): JSX.Element => {
 	const p = props.componentData.node;
 	return (
-		<span>
+		<>
 		<span>&lt;</span><span>{p.sequence}</span><span>&gt;</span>
-		</span>
+		</>
 	);
 };
 
