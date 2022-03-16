bd00e32cf854a0bcd3409bb068a16e4bf3023691
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-03-16 11:55:18 +0200

    remove comment

--- stylable.config.js
+++ stylable.config.js
@@ -4,13 +4,7 @@ module.exports.webpackPlugin = (defaultPluginOptions) => {
 		stylableConfig(defaultStylableConfig) {
 			return {
 				...defaultStylableConfig,
-				// Example override the namespace generation strategy
-				// resolveNamespace: (ns)=> `prefix-${ns}`
 			};
 		},
-		// Example to unsafely ignore duplicate namespace errors
-		// unsafeMuteDiagnostics: {
-		//   DUPLICATE_MODULE_NAMESPACE: 'warn'
-		// }
 	};
 };
