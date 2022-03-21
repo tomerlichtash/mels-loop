b1ce8c3e8496cb94f91bb9a83a12e527b0d8dfb6
dfl <dfl.github@gmail.com>
2022-03-21 11:03:27 +0200

    Added a script for forcing recompile of styles

---
+++ scripts/touch-css.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+CSS_SRC=src/components/layout/layout.st.css
+cp -f $CSS_SRC /tmp
+echo "" >> $CSS_SRC
+echo '.dummy { color: red; }' >> $CSS_SRC
+sleep 2
+cp -f $CSS_SRC /tmp/new.css
+cp -f /tmp/layout.st.css $CSS_SRC
+echo "Touched $CSS_SRC"
