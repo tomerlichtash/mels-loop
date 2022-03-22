0cddde0394bd06fb53f5a3adb736f4351ac51a19
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-03-22 19:59:23 +0200

    Added base colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
 						// crossorigin
 					></link>
 					<link
-						href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;500;700&family=Nunito:wght@300;500;700&display=swap"
+						href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
 						rel="stylesheet"
 					></link>
 				</Head>
