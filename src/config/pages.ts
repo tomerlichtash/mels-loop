45667fb353ad861e154979a74a369823a2295497
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-02 09:58:00 +0300

    cleanup pages utils

--- src/config/pages.ts
+++ src/config/pages.ts
@@ -1,4 +1,3 @@
-// import { SitePageRef } from "../interfaces/models";
 import { SITE_PAGES } from "./pages-data";
 import { SITE_META } from "../locales/components";
 
@@ -12,60 +11,3 @@ export const getSiteSubtitle = () => {
 
 export const getPathData = (id: string) =>
 	Object.values(SITE_PAGES).filter((p) => p.targetPathname === id)[0];
-
-// export const getPage = (id: string) => SITE_PAGES.filter((p) => p.id === id);
-
-// export const getPageRefs = () =>
-// 	SITE_PAGES.map(({ id, menuNav }: SitePageRef) => {
-// 		return { id, menuNav };
-// 	});
-
-// export const isPageVisible = (id: string) => {
-// 	const page = getPage(id);
-// 	if (!page.length) {
-// 		throw new Error(`Missing page key: ${id}`);
-// 	}
-// 	if (!Object.keys(page[0]).includes("menuNav")) {
-// 		throw new Error(`Missing visibility settings for pageId ${id}`);
-// 	}
-// 	const { menuNav } = page[0];
-// 	return menuNav;
-// };
-
-// export const getPagePath = (id: string) => {
-// 	const page = getPage(id);
-// 	if (!page.length) {
-// 		throw new Error(`Missing page key: ${id}`);
-// 	}
-// 	const { targetPathname } = page[0];
-// 	if (!targetPathname) {
-// 		throw new Error(`Missing targetPathname for pageId ${id}`);
-// 	}
-// 	return targetPathname;
-// };
-
-// export const getPageName = (id: string) => {
-// 	const page = getPage(id);
-// 	if (!page.length) {
-// 		throw new Error(`Missing page key: ${id}`);
-// 	}
-// 	const { locale } = page[0];
-// 	if (!locale) {
-// 		throw new Error(`Missing locale for pageId ${id}`);
-// 	}
-// 	return locale.pageName;
-// };
-
-// export const isCurrentPage = (
-// 	id: string,
-// 	currentPageId: string,
-// 	pageParent: string
-// ): boolean => {
-// 	if (pageParent) {
-// 		const pageData = getPage(pageParent)[0];
-// 		if (!pageData?.children?.includes(id)) {
-// 			return pageParent === id;
-// 		}
-// 	}
-// 	return currentPageId === id;
-// };
