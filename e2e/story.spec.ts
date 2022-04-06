126677a20a1430500a269304e6734baa1d27d322
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-06 19:11:34 +0300

    Cleanup tests and make Jest run again

--- e2e/story.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("should navigate to the story page", async ({ page }) => {
-	await page.goto("/");
-	const storyLink = page.locator("nav a:has-text('The Story')");
-	await storyLink.click();
-	await expect(page).toHaveURL("/story");
-	await expect(page.locator("h1")).toContainText("The Story of Mel");
-});
