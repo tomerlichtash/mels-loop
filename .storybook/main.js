9c54c9b22d61ad6245a73f4baf519ef82613e688
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-10 07:39:13 +0300

    Add Storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ const {
 
 module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: [],
+	addons: ["storybook-addon-next-router"],
 	framework: "@storybook/react",
 	core: {
 		builder: "webpack5",
