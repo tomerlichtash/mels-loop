b1738e5cad435cbf56e9ce4570afe0c294c2f1b7
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-10 07:39:13 +0300

    Add Storybook configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,17 @@
+import { RouterContext } from "next/dist/shared/lib/router-context"; // next 12
+
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
-	layout: "centered",
+	layout: "fullscreen",
 	controls: {
 		matchers: {
 			color: /(background|color)$/i,
 			date: /Date$/,
 		},
+	},
+	nextRouter: {
+		Provider: RouterContext.Provider,
+		locales: ["en", "he"],
+		locale: "en",
 	},
 };
