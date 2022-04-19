23f7fad5a8800aed7303427e13575828db465a4b
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-19 17:53:44 +0300

    testing2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link
 						rel="preconnect"
 						href="https://fonts.gstatic.com"
@@ -15,7 +15,7 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;500;600;700&family=Roboto+Slab:wght@400;500;600;700&display=swap"
 						rel="stylesheet"
 						crossOrigin="anonymous"
-					/>
+					/> */}
 				</Head>
 				<body>
 					<Main />
