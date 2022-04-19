0706ee30d8b05e6228182b54b69b23a849966dbc
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-19 12:43:03 +0300

    Move font loading back to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
 						href="https://fonts.gstatic.com"
 						// crossorigin
 					></link>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;500;600;700&family=Roboto+Slab:wght@400;500;600;700&display=swap"
+						rel="stylesheet"
+					></link>
 				</Head>
 				<body>
 					<Main />
