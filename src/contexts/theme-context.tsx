22a1519dcc5d02730c9e44adf83ea2c69c836f01
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-20 09:03:28 +0300

    Return null from ThemeCtx - test

--- src/contexts/theme-context.tsx
+++ src/contexts/theme-context.tsx
@@ -20,7 +20,7 @@ export class ThemeContext implements IThemeContext {
 	private _setLocalStorage: (theme: Themes) => void;
 	constructor(props: IThemeContextProps) {
 		if (!props) {
-			return;
+			return null;
 		}
 		const { theme, setTheme, setLSTheme } = props;
 		this._theme = theme;
@@ -31,7 +31,7 @@ export class ThemeContext implements IThemeContext {
 	private setTheme(theme: Themes) {
 		localStorage.clear();
 		this._setState(theme);
-		// this._setLocalStorage(theme);
+		this._setLocalStorage(theme);
 	}
 
 	public get theme() {
