3f39a929cb1b0a7e8a793308859d556cebccc917
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-23 21:00:49 +0300

    refactor(context): Combine React Contexts  (#162)

--- src/contexts/query-context.tsx
+++ src/contexts/query-context.tsx
@@ -1,6 +1,7 @@
-import { Context, createContext } from "react";
+import React, { Context, createContext } from "react";
 import { IQueryContext } from "../interfaces/query-context";
 import { IQueryManager } from "../interfaces/query-manager";
+import { QueryManager } from "./query-manager";
 
 export class QueryContext implements IQueryContext {
 	constructor(public readonly query: IQueryManager) {}
@@ -9,3 +10,11 @@ export class QueryContext implements IQueryContext {
 const ctx = createContext<IQueryContext>(new QueryContext(null));
 
 export const ReactQueryContext: Context<IQueryContext> = ctx;
+
+export const QueryContextProvider = ({ children, router }) => (
+	<ReactQueryContext.Provider
+		value={new QueryContext(new QueryManager({ router }))}
+	>
+		{children}
+	</ReactQueryContext.Provider>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,14 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import { LocaleContext, ReactLocaleContext } from "../contexts/locale-context";
-import { QueryContext, ReactQueryContext } from "../contexts/query-context";
-import { PageContext, ReactPageContext } from "../contexts/page-context";
-import { DynamicContentServer } from "../lib/dynamic-content-server";
-import { QueryManager } from "../contexts/query-manager";
-import { ThemeContextProvider } from "../contexts/theme-context";
+import { AppContext } from "../contexts";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	const router = useRouter();
-	const queryContext = new QueryContext(new QueryManager({ router }));
-	const localeContext = new LocaleContext({ router });
-	const contentContext = new PageContext(new DynamicContentServer());
-
 	return (
-		<ReactQueryContext.Provider value={queryContext}>
-			<ReactLocaleContext.Provider value={localeContext}>
-				<ReactPageContext.Provider value={contentContext}>
-					<ThemeContextProvider>
-						<Component {...pageProps} />
-					</ThemeContextProvider>
-				</ReactPageContext.Provider>
-			</ReactLocaleContext.Provider>
-		</ReactQueryContext.Provider>
+		<AppContext router={router}>
+			<Component {...pageProps} />
+		</AppContext>
 	);
 };
 
