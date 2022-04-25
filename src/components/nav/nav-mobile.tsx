7794f03908296a02133c0c344027d2523ad22e9c
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-04-25 08:37:32 +0300

    Clutter Cleanup

--- src/components/nav/nav-mobile.tsx
+++ src/components/nav/nav-mobile.tsx
@@ -4,15 +4,19 @@ import LocaleSelector from "../locale-selector";
 import Nav from "../nav";
 import { push as Menu } from "react-burger-menu";
 import { st, classes } from "./nav-mobile.st.css";
+import ThemeSelector from "../theme-selector";
+import Header from "../header";
 
 export interface IMobileNavProps extends ComponentProps {
 	onLocaleChange: (id: string) => Promise<boolean>;
+	isHome: boolean;
 	right: boolean;
 }
 
 export const MobileNav = ({
 	onLocaleChange,
 	right,
+	isHome,
 	className,
 }: IMobileNavProps): JSX.Element => {
 	return (
@@ -41,10 +45,12 @@ export const MobileNav = ({
 			right={right}
 			className={st(classes.root, className)}
 		>
+			<Header isHome={isHome} />
 			<LocaleSelector
 				onLocaleChange={onLocaleChange}
 				className={classes.localeSelector}
 			/>
+			<ThemeSelector />
 			<Nav className={st(classes.nav, { right })} />
 		</Menu>
 	);
