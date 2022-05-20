d9cc874140d298a3eed2789c7c99ce239c9306c3
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-05-20 07:00:05 +0300

    Typography fixes (#213)

--- src/components/bibliography/bibliography.tsx
+++ src/components/bibliography/bibliography.tsx
@@ -22,20 +22,21 @@ export const Bibliography = ({
 }: IBibliographyProps): JSX.Element => {
 	return (
 		<div className={st(classes.root, className)}>
-			<div className={classes.title}>{label}</div>
-			<span className={classes.list}>
+			<div className={classes.title}>{label}:</div>
+			<ul className={classes.list}>
 				{sources.map(({ name, url }) => {
 					return (
-						<Button
-							key={mlUtils.uniqueId()}
-							label={name}
-							link={url}
-							target="_blank"
-							className={classes.button}
-						/>
+						<li key={mlUtils.uniqueId()}>
+							<Button
+								label={name}
+								link={url}
+								target="_blank"
+								className={classes.button}
+							/>
+						</li>
 					);
 				})}
-			</span>
+			</ul>
 		</div>
 	);
 };
