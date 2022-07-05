e9c92bc708b1972d5eeb20422c0d0b7652225f5c
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-07-05 16:09:11 +0300

    Tidy up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
 import { classes as LightTheme } from "../theme/light/style.st.css";
-// import { classes as DarkTheme } from "../theme/dark/style.st.css";
-// import Cookies from "js-cookie";
-
-// const storedTheme = Cookies.get("theme");
 
 const themes = {
 	light: [LightTheme.root],
-	// dark: [DarkTheme.root],
-	// base: [],
 };
 
 class MyDocument extends Document {
@@ -41,17 +34,6 @@ class MyDocument extends Document {
 						as="font"
 						type="font/woff2"
 					/>
-					{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
-						crossOrigin="anonymous"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;500;600;700&family=Roboto+Slab:wght@400;500;600;700&display=swap"
-						rel="stylesheet"
-						crossOrigin="anonymous"
-					/> */}
 				</Head>
 				<body className={themes.light[0]}>
 					<Main />
