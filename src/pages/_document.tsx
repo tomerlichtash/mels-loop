22f2da369454765d8ead388de4a109772c786ed7
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-07-09 19:36:04 +0300

    cleanup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,16 +10,6 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
-						crossOrigin="anonymous"
-					/>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;700&display=swap"
-						rel="stylesheet"
-					/> */}
 					<link
 						rel="preload"
 						href="./assets/fonts/roboto-slab/RobotoSlab-Regular.woff2"
@@ -49,7 +39,7 @@ class MyDocument extends Document {
 						crossOrigin="anonymous"
 					/>
 				</Head>
-				<body className={themes.light[0]} style={{ fontFamily: "Roboto Slab" }}>
+				<body className={themes.light[0]}>
 					<Main />
 					<NextScript />
 				</body>
