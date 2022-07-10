b484f133420d2cc0749566eef1a75904c4070935
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-07-10 13:48:34 +0300

    fix(render): Fix onload flickering and stylable optimization issues (#251)
    * Fix Stylable optimizer
    * Use Local fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,45 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { classes as LightTheme } from "../theme/light/style.st.css";
+
+const themes = {
+	light: [LightTheme.root],
+};
 
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
+						rel="preload"
+						href="./assets/fonts/roboto-slab/RobotoSlab-Regular.woff2"
+						as="font"
+						type="font/woff2"
 						crossOrigin="anonymous"
 					/>
 					<link
-						href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;500;600;700&family=Roboto+Slab:wght@400;500;600;700&display=swap"
-						rel="stylesheet"
+						rel="preload"
+						href="./assets/fonts/roboto-slab/RobotoSlab-Medium.woff2"
+						as="font"
+						type="font/woff2"
+						crossOrigin="anonymous"
+					/>
+					<link
+						rel="preload"
+						href="./assets/fonts/roboto-slab/RobotoSlab-SemiBold.woff2"
+						as="font"
+						type="font/woff2"
+						crossOrigin="anonymous"
+					/>
+					<link
+						rel="preload"
+						href="./assets/fonts/roboto-slab/RobotoSlab-Bold.woff2"
+						as="font"
+						type="font/woff2"
 						crossOrigin="anonymous"
 					/>
 				</Head>
-				<body>
+				<body className={themes.light[0]}>
 					<Main />
 					<NextScript />
 				</body>
