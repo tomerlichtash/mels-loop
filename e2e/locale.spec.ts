463e478a0b92c00442d98da4646ab3bc811eed14
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-07-22 17:19:48 +0300

    No same-locale navigation spec

--- e2e/locale.spec.ts
+++ e2e/locale.spec.ts
@@ -1,15 +1,31 @@
 import { test, expect } from "@playwright/test";
 
-test("should switch language to RTL", async ({ page }) => {
-	await page.goto("http://localhost:3000/");
-	await page.click(`[data-test-id="Button_LocaleSelector_he"]`);
-	await expect(page).toHaveURL("http://localhost:3000/he");
-	await expect(page.locator("h1")).toHaveText("הסיפור על מל");
-});
+test.describe("Locale Selector", () => {
+	test("should switch language to RTL", async ({ page }) => {
+		await page.goto("http://localhost:3000/");
+		await page.click(`[data-test-id="Button_LocaleSelector_he"]`);
+		await expect(page).toHaveURL("http://localhost:3000/he");
+		await expect(page.locator("h1")).toHaveText("הסיפור על מל");
+	});
 
-test("should switch language to LTR", async ({ page }) => {
-	await page.goto("http://localhost:3000/he");
-	await page.click(`[data-test-id="Button_LocaleSelector_en"]`);
-	await expect(page).toHaveURL("http://localhost:3000");
-	await expect(page.locator("h1")).toHaveText("The Story of Mel");
+	test("should switch language to LTR", async ({ page }) => {
+		await page.goto("http://localhost:3000/he");
+		await page.click(`[data-test-id="Button_LocaleSelector_en"]`);
+		await expect(page).toHaveURL("http://localhost:3000");
+		await expect(page.locator("h1")).toHaveText("The Story of Mel");
+	});
+
+	test("[en] should not navigate to same language", async ({ page }) => {
+		await page.goto("http://localhost:3000/");
+		await page.click(`[data-test-id="Button_LocaleSelector_en"]`);
+		await expect(page).toHaveURL("http://localhost:3000/");
+		await expect(page.locator("h1")).toHaveText("The Story of Mel");
+	});
+
+	test("[he] should not navigate to same language", async ({ page }) => {
+		await page.goto("http://localhost:3000/he");
+		await page.click(`[data-test-id="Button_LocaleSelector_he"]`);
+		await expect(page).toHaveURL("http://localhost:3000/he");
+		await expect(page.locator("h1")).toHaveText("הסיפור על מל");
+	});
 });
