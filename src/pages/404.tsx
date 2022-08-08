96e996839148decdb9428dd13e6c6b96a90a1d0f
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-08-08 10:12:41 +0300

    Remove comments

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,14 +14,6 @@ const Error404: NextPage = () => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-	/**
-	 * Yep, it's empty, but needed to force reading cookies and request
-	 * headers on server-side so _document can read it
-	 *
-	 * See references:
-	 * https://github.com/vercel/next.js/discussions/18235
-	 * https://eric-schaefer.com/til/2022/02/05/next.js-unexpected-missing-ctx.req-in-_document/
-	 */
 	return { props: {} };
 };
 
