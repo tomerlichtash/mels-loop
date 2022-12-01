ee1243ac927076de03f86c652bf8828e360b465d
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-12-01 11:34:03 +0200

    Fix mobile spec definition

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,12 @@ const config: PlaywrightTestConfig = {
 		// Test against mobile viewports.
 		{
 			name: "Mobile Chrome",
-			testMatch: [/.mobile.spec.ts/, /codex.spec.ts/, /popover.spec.ts/],
+			testMatch: [
+				/.mobile.spec.ts/,
+				/glossary.spec.ts/,
+				/annotations.spec.ts/,
+				/popover.spec.ts/,
+			],
 			use: {
 				...devices["Pixel 5"],
 			},
