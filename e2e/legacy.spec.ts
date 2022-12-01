d5822fa9332168325ef84940d57c4dc00266799d
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-12-01 12:58:29 +0200

    Add spec for leagcy path names

---
+++ e2e/legacy.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+import {
+	getLocalePath,
+	locales,
+	getFrontMatter,
+	translate,
+} from "./utils/test-utils";
+
+test.describe("Legacy Paths", () => {
+	const legacyDocs = ["preface", "mels-hack-the-missing-bits", "resources"];
+	legacyDocs.map((legacyDocName) => {
+		locales.map((locale) => {
+			test(`${locale} > should support single doc URL to ${legacyDocName}`, async ({
+				page,
+			}) => {
+				const docId = "docs/the-story-of-mel";
+				const filename = "index";
+				const { data } = getFrontMatter(
+					docId,
+					`pages/${legacyDocName}/${filename}`,
+					locale
+				);
+				await page.goto(`localhost:3000/${locale}/docs/${legacyDocName}`);
+				await expect(page).toHaveURL(
+					getLocalePath(locale, docId, `pages/${legacyDocName}`)
+				);
+				await expect(page.locator("h1")).toHaveText(data.title as string);
+			});
+		});
+	});
+});
