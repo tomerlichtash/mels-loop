26ffcee1562a9770ac357aaae58eaf5a7ef997ce
dfl <dfl.github@gmail.com>
2022-12-04 11:49:54 +0200

    removed redirects

--- next.config.js
+++ next.config.js
@@ -46,31 +46,7 @@ const nextConfig = {
 	},
 	serverRuntimeConfig: {
 		PROJECT_ROOT: __dirname,
-	},
-	async redirects() {
-		return [
-			{
-				source: "/docs/preface",
-				destination: "/docs/the-story-of-mel/pages/preface",
-				permanent: true,
-			},
-			{
-				source: "/docs/mels-hack-the-missing-bits",
-				destination: "/docs/the-story-of-mel/pages/mels-hack-the-missing-bits",
-				permanent: true,
-			},
-			{
-				source: "/docs/resources",
-				destination: "/docs/the-story-of-mel/pages/resources",
-				permanent: true,
-			},
-			{
-				source: "/docs/blackjack-writeup",
-				destination: "/docs/the-story-of-mel/pages/blackjack-writeup",
-				permanent: true,
-			},
-		];
-	},
+	}
 };
 
 module.exports = withAxiom(nextConfig);
