dc86970e23c6d047a45d3cc516bbf23790727dbb
Tomer Lichtash <tomerlichtash@gmail.com>
2022-12-06 11:42:58 +0200

    fail with only articles

--- e2e/articles.spec.ts
+++ e2e/articles.spec.ts
@@ -6,7 +6,7 @@ import {
 	translate,
 } from "./utils/test-utils";
 
-test.describe("Articles", () => {
+test.describe.only("Articles", () => {
 	const docId = "docs/the-story-of-mel";
 	locales.map((locale) => {
 		test(`${locale} > should navigate to the Preface article`, async ({
