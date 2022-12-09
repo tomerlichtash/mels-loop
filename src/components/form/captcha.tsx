11041b04fee61c8acb688e5d69b3dce867a5ca95
dfl <dfl.github@gmail.com>
2022-12-09 22:21:36 +0200

    wrapped more `fetch` in `try/catch`

--- src/components/form/captcha.tsx
+++ src/components/form/captcha.tsx
@@ -4,27 +4,41 @@ import ReCAPTCHA from "react-google-recaptcha";
 import { ICaptchaProps } from "./types";
 import { st, classes } from "./captcha.st.css";
 
-export const fetchCaptcha = (value: string) =>
-	fetch("/api/captcha", {
-		body: JSON.stringify({ value }),
-		headers: { "Content-Type": "application/json" },
-		method: "POST",
-	});
+/**
+ * Swallows errors
+ * @param value 
+ * @returns 
+ */
+export const fetchCaptcha = async (value: string) => {
+	try {
+		const res = await fetch("/api/captcha", {
+			body: JSON.stringify({ value }),
+			headers: { "Content-Type": "application/json" },
+			method: "POST",
+		});
+		if (res.status !== 200) {
+			return { error: `Bad status ${res.status}` };
+		}
+		return await res.json();
+	}
+	catch (e) {
+		return { error: String(e) }
+	}
+}
 
 export const onCaptchaChange = async (
 	value: string,
 	onSuccess: () => void
-): Promise<Response | false> => {
-	const res = await fetchCaptcha(value);
-	const { error } = await res.json();
+): Promise<boolean> => {
+	const { error } = await fetchCaptcha(value);
 	if (error) {
 		console.error(error);
+		return false;
 	}
-	if (res.status === 200) {
+	else {
 		onSuccess();
-		return res;
+		return true;
 	}
-	return false;
 };
 
 export const Captcha = ({
@@ -43,19 +57,19 @@ export const Captcha = ({
 		return <div className={classes.error}></div>
 	}
 	return (
-	<div className={st(classes.root, { highlight }, className)}>
-		<div className={classes.captcha} tabIndex={tabIndex}>
-			<ReCAPTCHA
-				sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
-				// eslint-disable-next-line @typescript-eslint/no-misused-promises
-				onChange={(value: string) =>
-					onCaptchaChange(value, () => onChange(value))
-				}
-				onExpired={onExpired}
-				size="normal"
-				hl={locale}
-				theme={theme === "dark" ? "dark" : "light"}
-			/>
-		</div>
-	</div>);
+		<div className={st(classes.root, { highlight }, className)}>
+			<div className={classes.captcha} tabIndex={tabIndex}>
+				<ReCAPTCHA
+					sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY}
+					// eslint-disable-next-line @typescript-eslint/no-misused-promises
+					onChange={(value: string) =>
+						onCaptchaChange(value, () => onChange(value))
+					}
+					onExpired={onExpired}
+					size="normal"
+					hl={locale}
+					theme={theme === "dark" ? "dark" : "light"}
+				/>
+			</div>
+		</div>);
 }
