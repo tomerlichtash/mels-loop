b2561532b96881b14b7439744337008432701355
Tomer Lichtash <tomerlichtash@users.noreply.github.com>
2022-12-14 08:33:53 +0200

    Rename IFetchParams

--- src/config/public-api-params.ts
+++ src/config/public-api-params.ts
@@ -1,10 +1,10 @@
-export interface ISendgridSubmitAPI {
+export interface IFetchParams {
 	path: string;
 	headers: Record<string, string>;
 	method: "POST" | "GET" | "PUT" | "OPTIONS";
 }
 
-export const SUBMIT_API: Record<string, ISendgridSubmitAPI> = {
+export const SUBMIT_API: Record<string, IFetchParams> = {
 	SEND_GRID: {
 		path: "/api/sendgrid",
 		headers: { "Content-Type": "application/json" },
