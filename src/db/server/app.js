a0e17b677f78c8a91de14c0ff5bfc87c0a42b42a
dfl <dfl.github@gmail.com>
2023-02-24 02:06:10 +0200

    Added db server test with article posting

--- src/db/server/app.js
+++ src/db/server/app.js
@@ -5,11 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const koa_1 = __importDefault(require("koa"));
 const db_service_1 = require("./db-service");
+const test_db_server_1 = require("./test/test-db-server");
 const Router = require("@koa/router");
+const KoaJSON = require("koa-json");
+const KoaBodyParser = require("koa-body-parser");
 class MLDBApp {
     constructor() {
         this._port = 0;
         this._app = new koa_1.default();
+        this._app.use(KoaJSON());
+        this._app.use(KoaBodyParser());
         this._dbService = (0, db_service_1.createServerDB)({});
     }
     get port() {
@@ -26,7 +31,8 @@ class MLDBApp {
                 ctx.body = { ok: true };
             });
             router.put("/article", async (ctx) => {
-                const { article } = ctx.body;
+                const body = ctx.request.body, req = ctx.req;
+                const { article } = body;
                 if (!article || typeof article !== "object") {
                     ctx.status = 500;
                     ctx.body = { error: "data should be an object with an article field containing the article" };
@@ -51,8 +57,10 @@ class MLDBApp {
 }
 const app = new MLDBApp();
 app.run()
-    .then(() => {
+    .then(async () => {
     console.log(`App listening on port ${app.port}`);
+    const errors = await (0, test_db_server_1.testDB)(app.port);
+    console.log("Test errors", errors.join('\n'));
 })
     .catch(err => console.error(`ML DB App failed: ${err}`));
 //# sourceMappingURL=app.js.map
--- src/db/server/app.ts
+++ src/db/server/app.ts
@@ -3,66 +3,73 @@ import { Context } from "koa";
 import * as KoaRouter from "koa-router";
 import { createServerDB, IDBService } from "./db-service";
 import { DB_MODELS } from "../models";
+import { testDB } from "./test/test-db-server";
 
 const Router = require("@koa/router");
+const KoaJSON = require("koa-json");
+const KoaBodyParser = require("koa-body-parser");
 
 
 class MLDBApp {
-    private readonly _app: Koa;
-    private readonly _dbService: IDBService;
+	private readonly _app: Koa;
+	private readonly _dbService: IDBService;
 
-    constructor() {
-        this._app = new Koa();
-        this._dbService = createServerDB({});
-    }
+	constructor() {
+		this._app = new Koa();
+		this._app.use(KoaJSON());
+		this._app.use(KoaBodyParser());
+		this._dbService = createServerDB({});
+	}
 
 
-    public get port(): number {
-        return this._port;
-    }
+	public get port(): number {
+		return this._port;
+	}
 
-    private _port: number = 0;
+	private _port: number = 0;
 
-    public async run(): Promise<string> {
-        try {
-            const inited = await this._dbService.init();
-            if (!inited) {
-                return ("failed to init db");
-            }
-            const router: KoaRouter = new Router();
-            router.get("/connect", (ctx: Context) => {
-                ctx.body = { ok: true };
-            });
+	public async run(): Promise<string> {
+		try {
+			const inited = await this._dbService.init();
+			if (!inited) {
+				return ("failed to init db");
+			}
+			const router: KoaRouter = new Router();
+			router.get("/connect", (ctx: Context) => {
+				ctx.body = { ok: true };
+			});
 
-            router.put("/article", async (ctx: Context) => {
-                const { article }= ctx.body as { article: DB_MODELS.IArticleData };
-                if (!article || typeof article !== "object") {
-                    ctx.status = 500;
-                    ctx.body = { error: "data should be an object with an article field containing the article"};
-                    return;
-                }
+			router.put("/article", async (ctx: Context) => {
+				const body = (ctx.request as any).body,
+					req = ctx.req;
+				const { article } = body as { article: DB_MODELS.IArticleData };
+				if (!article || typeof article !== "object") {
+					ctx.status = 500;
+					ctx.body = { error: "data should be an object with an article field containing the article" };
+					return;
+				}
 
-                const saved = await this._dbService.save({
-                    table: "articles",
-                    data: article
-                })
-            })
+				const saved = await this._dbService.save({
+					table: "articles",
+					data: article
+				})
+			})
 
-            this._app.use(router.routes()).use(router.allowedMethods());
+			this._app.use(router.routes()).use(router.allowedMethods());
 
-            const envPort = process.env.DB_API_PORT;
-            this._port = (envPort && !isNaN(parseInt(envPort))) ?
-                parseInt(envPort) : 11012;
+			const envPort = process.env.DB_API_PORT;
+			this._port = (envPort && !isNaN(parseInt(envPort))) ?
+				parseInt(envPort) : 11012;
 
-            this._app.listen(this._port);
+			this._app.listen(this._port);
 
-            return "";
-        }
-        catch (err) {
-            return String(err);
-        }
+			return "";
+		}
+		catch (err) {
+			return String(err);
+		}
 
-    }
+	}
 }
 
 
@@ -70,7 +77,9 @@ class MLDBApp {
 const app = new MLDBApp();
 
 app.run()
-    .then(() => {
-        console.log(`App listening on port ${app.port}`);
-    })
-    .catch(err => console.error(`ML DB App failed: ${err}`));
+	.then(async () => {
+		console.log(`App listening on port ${app.port}`);
+		const errors = await testDB(app.port);
+		console.log("Test errors", errors.join('\n'))
+	})
+	.catch(err => console.error(`ML DB App failed: ${err}`));
