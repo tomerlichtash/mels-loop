828ca2f1a5f3dbfec666451f5ba49a114a5ff448
Tomer Lichtash <tomer@descope.com>
2023-06-17 12:26:06 +0300

    Cleanup

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -1,6 +1,3 @@
-// import { classes as LightTheme } from "../theme/light/style.st.css";
-// import { classes as DarkTheme } from "../theme/dark/style.st.css";
-
 export type Themes = "base" | "light" | "dark" | null;
 
 export const themes: Record<string, string[]> = {
