76e6e90a90ab7c86a084164fb8534c1d017c7baf
dfl <dfl.github@gmail.com>
2023-06-21 01:32:13 +0300

    PR change requests + cleanup and better listing

--- scripts/list-media.ts
+++ scripts/list-media.ts
@@ -1,31 +1,33 @@
 
 
-import {
-    ListObjectsV2Command
-} from "@aws-sdk/client-s3";
+import { ListObjectsV2Command, ListObjectsV2CommandOutput,
+	_Object as S3Object } from "@aws-sdk/client-s3";
 import { createS3Proxy } from "./s3-client";
 
+// runs without params to list the content of the bucket configured in .env
+
+const proxy = createS3Proxy();
 
 const listObjects = async () => {
-    const proxy = createS3Proxy();
-    const cmd = new ListObjectsV2Command({
-        Bucket: proxy.bucket,
+	const cmd = new ListObjectsV2Command({
+		Bucket: proxy.bucket,
 
-    });
-    const response = await proxy.client.send(cmd);
-    return response;
+	});
+	const response = await proxy.client.send(cmd);
+	return response;
 }
 
-
-
-listObjects().then((response: any) => {
-    console.log("List response", response);
+listObjects().then((response: ListObjectsV2CommandOutput) => {
+	console.log("List response", response.Contents?.map(o => ({
+		name: o.Key,
+		size: ((o.Size || 0)/1000).toFixed(2) + "KB",
+		url: proxy.getObjectUrl(o.Key!)
+	})));
 })
-    .catch((err) => {
-        console.error(err);
-    })
-    .finally(() => {
-        console.log(`Done listing`);
-        process.exit();
-    });
+	.catch((err) => {
+		console.error(err);
+	})
+	.finally(() => {
+		process.exit();
+	});
 
