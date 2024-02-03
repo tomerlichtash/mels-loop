c4074c2df433c945de9d4b954caf6c28e368f905
Tomer Lichtash <tomerlichtash@gmail.com>
2024-02-03 21:34:50 +0200

    store report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
-import { PlaywrightTestConfig, devices } from "@playwright/test";
-import path from "path";
-import { fileURLToPath } from "url";
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,45 +10,46 @@ const baseURL = `http://localhost:${PORT}`;
 
 const config: PlaywrightTestConfig = {
 	reporter: [
-		process.env.CI ? ["junit", { outputFile: "results.xml" }] : ["list"],
+		process.env.CI ? ['list', 'html'] : ['list', 'html'],
+		// process.env.CI ? ['junit', { outputFile: 'results.xml' }] : ['list'],
 	],
 	timeout: 30 * 1000,
-	testDir: path.join(__dirname, "e2e"),
+	testDir: path.join(__dirname, 'e2e'),
 	retries: 2,
-	outputDir: "test-results/",
+	outputDir: 'test-results/',
 	workers: 4,
 	fullyParallel: true,
 	webServer: {
-		command: "yarn run dev",
+		command: 'yarn run dev',
 		url: baseURL,
 		timeout: 120 * 1000,
 		reuseExistingServer: !process.env.CI,
 	},
 	use: {
-		video: "retain-on-failure",
+		video: 'retain-on-failure',
 		baseURL,
-		trace: "retry-with-trace",
+		trace: 'retry-with-trace',
 	},
 	projects: [
 		{
-			name: "Site",
-			testMatch: ["site/**/*.spec.ts"],
+			name: 'Site',
+			testMatch: ['site/**/*.spec.ts'],
 			use: {
-				...devices["Desktop Chrome"],
+				...devices['Desktop Chrome'],
 			},
 		},
 		{
-			name: "Glossary",
-			testMatch: ["/glossary/**/*.spec.ts"],
+			name: 'Glossary',
+			testMatch: ['/glossary/**/*.spec.ts'],
 			use: {
-				...devices["Desktop Chrome"],
+				...devices['Desktop Chrome'],
 			},
 		},
 		{
-			name: "codex-the-story-of-mel",
-			testMatch: ["codex/the-story-of-mel/**/*.spec.ts"],
+			name: 'codex-the-story-of-mel',
+			testMatch: ['codex/the-story-of-mel/**/*.spec.ts'],
 			use: {
-				...devices["Desktop Chrome"],
+				...devices['Desktop Chrome'],
 			},
 		},
 	],
