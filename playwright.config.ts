3de76e84e9e7a74feffe1df45d6bd6fcb93ad5e8
Tomer Lichtash <tomerlichtash@gmail.com>
2024-02-16 15:53:05 +0200

    decrease workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ const config: PlaywrightTestConfig = {
 	testIgnore: 'fixme/*',
 	retries: 2,
 	outputDir: 'test-results/',
-	workers: 6,
+	workers: 3,
 	fullyParallel: true,
 	webServer: {
 		command: 'yarn run dev',
