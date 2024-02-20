f0bbf2e0b876ea9b77abe836507eb1024b3fd5f2
Tomer Lichtash <tomerlichtash@gmail.com>
2024-02-20 12:17:20 +0200

    fix stylelint comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,21 +8,25 @@ import 'normalize.css/normalize.css';
 import 'styles/app.scss';
 import type { IPageProps } from 'types/models';
 
-const App = ({ Component, pageProps }: AppProps<IPageProps>) => (
-	<ThemeProvider
-		defaultTheme="light"
-		storageKey="ml-theme"
-		attribute="data-ml-theme"
-	>
-		<PageProvider documentPath={pageProps.documentPath}>
-			<style jsx global>
-				{css`
-					${fontFaceDecls}
-				`}
-			</style>
-			<Component {...pageProps} />
-		</PageProvider>
-	</ThemeProvider>
-);
+const App = ({ Component, pageProps }: AppProps<IPageProps>) => {
+	const fontStyles = css`
+		${fontFaceDecls}
+	`;
+
+	return (
+		<ThemeProvider
+			defaultTheme="light"
+			storageKey="ml-theme"
+			attribute="data-ml-theme"
+		>
+			<PageProvider documentPath={pageProps.documentPath}>
+				<style jsx global>
+					{fontStyles}
+				</style>
+				<Component {...pageProps} />
+			</PageProvider>
+		</ThemeProvider>
+	);
+};
 
 export default App;
