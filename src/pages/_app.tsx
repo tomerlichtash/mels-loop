a681d2009eec01a336f50958d619ee01423adb05
Tomer Lichtash <tomerlichtash@gmail.com>
2024-02-20 09:47:22 +0200

    cleanup app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,27 @@ import React from 'react';
 import css from 'styled-jsx/css';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { PageProvider } from 'lib/dynamic-content-utils/context/pageContext';
 import { fontFaceDecls } from '../siteFonts';
-import PageProvider from 'lib/dynamic-content-utils/context/pageContext';
 import 'normalize.css/normalize.css';
 import 'styles/app.scss';
 import type { IPageProps } from 'types/models';
 
-const App = ({ Component, pageProps }: AppProps<IPageProps>) => {
-	const fontStyles = css`
-		${fontFaceDecls}
-	`;
-
-	return (
-		<ThemeProvider
-			defaultTheme="light"
-			storageKey="ml-theme"
-			attribute="data-ml-theme"
-		>
-			<PageProvider documentPath={pageProps.documentPath}>
-				<style jsx global>
-					{fontStyles}
-				</style>
-				<Component {...pageProps} />
-			</PageProvider>
-		</ThemeProvider>
-	);
-};
+const App = ({ Component, pageProps }: AppProps<IPageProps>) => (
+	<ThemeProvider
+		defaultTheme="light"
+		storageKey="ml-theme"
+		attribute="data-ml-theme"
+	>
+		<PageProvider documentPath={pageProps.documentPath}>
+			<style jsx global>
+				{css`
+					${fontFaceDecls}
+				`}
+			</style>
+			<Component {...pageProps} />
+		</PageProvider>
+	</ThemeProvider>
+);
 
 export default App;
